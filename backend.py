e0b5ad391595e70070e1e885bae8fec14e4cb441
Kaan Oflaz <59467007+Turkish-coffee@users.noreply.github.com>
2022-12-14 19:14:24 +0300

    kaan test

--- backend.py
+++ backend.py
@@ -1,9 +1,37 @@
 
 from pymongo import MongoClient
 
+
+
+
 #################################################################
 
 # Team 1
+
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://www.cybertek.fr']
+
+    def parse(self, response):
+        for title in response.css('.oxy-post-title'):
+            yield {'title': title.css('::text').get()}
+
+        for next_page in response.css('a.next'):
+            yield response.follow(next_page, self.parse)
+
+
+
+
+
+
+
+
+
+
+
+
 
 #################################################################
 
