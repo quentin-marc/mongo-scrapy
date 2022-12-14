6d75257fdc10729c7b329395c31802a55d6e54b6
quentin marc <quentinmarc81@gmail.com>
2022-12-14 18:21:12 +0300

    feat: initial backend file

---
+++ backend.py
@@ -0,0 +1,36 @@
+
+
+#################################################################
+
+# Team 1
+
+#################################################################
+
+
+data = [{
+    'url_image' : "https://www.cybertek.fr/images_produits/e0f01a81-4966-4612-ac8a-d75da145a79a.jpg",
+    'modele' : "MSI bim",
+    'prix' : 1200,
+    'utilisation' : "bureautique",
+    'resolution' : "4k",
+    'ram' : 8,
+    'constructeurGPU' : "Intel",
+    'tailleEcran' : 15,
+    'processeur' : "i5",
+    'tailleSD' : 256,
+    'chipSet' : "GF RTX"
+},{
+    'url_image' : "https://www.cybertek.fr/images_produits/e0f01a81-4966-4612-ac8a-d75da145a79a.jpg",
+    'modele' : "MSI bim 2",
+    'prix' : 700,
+    'utilisation' : "bureautique",
+    'resolution' : "4k",
+    'ram' : 8,
+    'constructeurGPU' : "Intel",
+    'tailleEcran' : 13,
+    'processeur' : "i5",
+    'tailleSD' : 512,
+    'chipSet' : "GF RTX"
+}]
+
+print(data)
