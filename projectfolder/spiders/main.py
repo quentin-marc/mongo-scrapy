aa230bccdd72cd661403e72cf38675c7dfbdc527
quentin marc <quentinmarc81@gmail.com>
2022-12-25 18:55:24 +0300

    scrapy connected to mongo

--- projectfolder/spiders/main.py
+++ projectfolder/spiders/main.py
@@ -1,5 +1,6 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from pymongo import MongoClient
 
 class CrawlingSpider(CrawlSpider):
     name = "mainCrawler"
@@ -14,12 +15,21 @@ class CrawlingSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        yield {
+        livre = {
             "title" : response.css(".product_main h1::text").get(),
             "price": response.css("div.product_main p.price_color::text").get().replace("\u00a3",""),
             "availability": response.css(".availability::text")[1].get().replace("\n","").replace("  ",""),
             "picture": response.css("div.item img::attr(src)").get().replace("../..","https://books.toscrape.com"),
             "description":response.css("article.product_page p::text")[10].get()
-            
         }
+        # database connection
+        client = MongoClient("mongodb://127.0.0.1:27018/")
+        mydb = client["scrapy-database"]
+        # get collection
+        db_collection = mydb['livres']
+        # key = {'title': livre.title}
+        key = {'title':livre["title"]}
+        inserted = db_collection.update_one(key, {"$set": livre}, upsert=True)
+
+        
     
